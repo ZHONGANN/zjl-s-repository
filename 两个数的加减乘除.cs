94f0ea92438395ac3d02c5bd955f31279fd91ba8
ZHONGANN <1356562448@qq.com>
2020-11-08 17:13:30 +0800

    Create 两个数的加减乘除.cs

---
+++ 两个数的加减乘除.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个数：");
+            string a = Console.ReadLine();
+            int num1 = Convert.ToInt32(a);
+            Console.WriteLine("请输入第二个数：");
+            string b = Console.ReadLine();
+            int num2 = Convert.ToInt32(b);
+            Console.WriteLine("两个数的和：{0}", num1 + num2);
+            Console.WriteLine("两个数的差：{0}", num1 - num2);
+            Console.WriteLine("两个数相乘：{0}", num1 * num2);
+            Console.WriteLine("两个数相除：{0}", (double)num1 / num2);
+            return 0;
+        }
+
+    }
+}
